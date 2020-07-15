b5f8af38c9b36868d2e24d8206c7a907d8b9a3d5
Zane Armstrong <zane.armstrong@comcast.net>
2020-07-15 09:38:25 -0500

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("clientside js file is loaded");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
